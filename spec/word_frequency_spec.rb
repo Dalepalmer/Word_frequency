b5081fdd6b91c8c4dc05c4adda63818b83e96e35
dizzjr <student@epicodus.com>
2015-01-09 16:29:09 -0800

    Changed tests desriptions

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -3,14 +3,14 @@ require("word_frequency")
 require("pry")
 
 describe ("String#word_frequency") do
-  it("counts how many times dog is in a give sentence") do
+  it("Tests the counting function for the word dog is in a give sentence") do
     expect(("dog").word_freq("A yellow dog dog dog dog")).to(eq(4))
   end
 
-  it ("counts sentences that are capitalized") do
+  it ("Tests the counting function of sentences that are capitalized") do
     expect(("blue").word_freq("Blue bLue blUe bluE BLue bLUe blUE BLUe bLUE")).to(eq(9))
   end
-  it ("uses capitalized words as an input") do
+  it ("Tests ability to understand capitalized words as an input") do
     expect(("BLUE").word_freq("blue")).to(eq(1))
   end
 end
